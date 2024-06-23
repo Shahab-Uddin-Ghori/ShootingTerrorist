d6bf6aaf6d901042aeead6f8d23cd28232b9cc6e
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-06-23 17:20:49 +0500

    add pic of ak 47

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// on load move with mouse
+document.addEventListener('DOMContentLoaded', (event) => {
+  const emoji = document.getElementById('emoji');
+
+  document.addEventListener('mousemove', (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      // Offset the emoji slightly so it doesn't sit directly under the cursor
+      const offsetX = 10;
+      const offsetY = 10;
+
+      emoji.style.transform = `translate(${mouseX + offsetX}px, ${mouseY + offsetY}px)`;
+  });
+});
+
+
+
+
 // Select the cursor element
 const cursor = document.querySelector(".cursor");
 
